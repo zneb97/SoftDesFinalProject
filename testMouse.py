7d982d01c2362e06efb10817d8678974b3619da9
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-03-20 20:07:38 -0400

    Mouse testing

---
+++ testMouse.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+try:
+    for i in range(10):
+        pyautogui.moveTo(100, 100, duration=0.50)
+        pyautogui.moveTo(1500, 100, duration=0.50)
+        pyautogui.moveTo(1500, 1500, duration=0.50)
+        pyautogui.moveTo(100, 1500, duration=0.50)
+except KeyboardInterrupt:
+    print("exit")
