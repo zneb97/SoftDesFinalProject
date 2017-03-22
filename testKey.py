ce64e798eb532db7560ed0431678cbd31d14d876
Ben Ziemann <benjamin.ziemann@students.olin.edu>
2017-03-22 00:32:03 -0400

    Libraries

---
+++ testKey.py
@@ -0,0 +1,11 @@
+import pyautogui
+
+for i in range(100):
+    pyautogui.press('w')     # press the F1
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
+    pyautogui.press('w')     # press the F1 key
