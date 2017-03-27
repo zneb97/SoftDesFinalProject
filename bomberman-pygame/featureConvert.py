ab12b4450ebf518301d81fbde95f6e7932e2e258
CleanestMink126 <steelman@students.olin.edu>
2017-03-27 18:31:48 -0400

    fixed one bug

--- bomberman-pygame/featureConvert.py
+++ bomberman-pygame/featureConvert.py
@@ -11,17 +11,19 @@ def convertGrid(grid, pPos):
 	viewY = 5
 	resultGridX = 2 * viewX - 1
 	resultGridY = 2 * viewY - 1
-	buckets = [ [0] * resultGridX ] * resultGridY
+	buckets = [ [0] * resultGridY ] * resultGridX
 	mymat = matrix(buckets)
 	x = 0
 	y = 0
-	for i in range(grid.shape[0] - pPos[0], 2 * grid.shape[0] - pPos[0]):
-		for j in range(grid.shape[1] - pPos[1], 2 * grid.shape[1] - pPos[1]):
-			mymat.itemset( (i, j) , grid.item((x, y)) )
+	for i in range(grid.shape[0] - pPos[0] - 1, 2 * grid.shape[0] - pPos[0] - 1):
+		for j in range(grid.shape[1] - pPos[1] - 1, 2 * grid.shape[1] - pPos[1] - 1):
+			mymat.itemset( (j, i) , grid.item((x, y)) )
 			print(str(i) + str(j))
 			y += 1
 		x += 1
 		y = 0
+
+	mymat.itemset((resultGridX/2, resultGridY/2), 5)
 
 	for i in range(0, mymat.shape[1]):
 		for j in range(0, mymat.shape[0]):
@@ -32,4 +34,4 @@ def convertGrid(grid, pPos):
 
 if __name__ == '__main__':
 	mygrid = [ [1] * 10 ] * 5
-	convertGrid(matrix(mygrid), (1,1))
+	convertGrid(matrix(mygrid), (0,9))
