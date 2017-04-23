2b60d392bf74950baa05d57479fbfd26972c74ae
Yichen Jiang <pauljyc0929@gmail.com>
2017-04-23 01:41:10 -0400

    Revert "modify title screen"
    This reverts commit ecfd149355038252b39ecf46faeb0b415c6c54d5, reversing
    changes made to 32f2efd47068688f9ad7cbda87179551516b0518.

--- bomberman-pygame/titlescreen.py
+++ bomberman-pygame/titlescreen.py
@@ -41,8 +41,14 @@ class Titlescreen():
 							self.singlePlayer() # Single Player game clicked
 						elif self.withinBoundary(25, 250, 550, 600):
 							userInteracted = True
-							self.highScores() # High Score clicked
+							self.multiplayer() # Multiplayer game clicked
 						elif self.withinBoundary(25, 240, 605, 645):
+							userInteracted = True
+							self.instructions() # Instructions clicked
+						elif self.withinBoundary(25, 240, 655, 700):
+							userInteracted = True
+							self.highScores() # High Scores clicked
+						elif self.withinBoundary(25, 105, 705, 745):
 							userInteracted = True
 							exitMain = True
 							pygame.quit() # Exit clicked
